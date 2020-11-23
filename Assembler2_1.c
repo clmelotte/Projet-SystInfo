029cd57c9edc47f49e55c14d543eae85b7a354e3
joseph <jtevanvan@gmail.com>
2020-11-23 21:46:56 +0100

    Philosophe sans print + fonctionne pour 1 (manque encore les erreurs !) Assembler en cours ...

--- Assembler2_1.c
+++ Assembler2_1.c
@@ -3,43 +3,75 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+int omega;
+int *mut;
+int *count;
 
 
-int main(int argc,char* agrv[]){
+long lock(int* mutexAdress){
+    int *adress= mutexAdress;
+    int output=0;
+    printf("non");
+/*
+    asm ("movl %0 , %%eax\n"
+         "xchgl %%eax, %1\n"
+         "movl %%eax, %0 "
+         :"=m"(output)  // y is output operand
+         :"m"(adress)   // x is input operand
+         :"eax"
+         ); // %eax is clobbered register
+         */
+    return output;
+}
+
+long unlock(int* mutexAdress){
+    int *adress= mutexAdress;
+    int output=0;
+    printf("oui");
+/*
+    asm("movl %0 , %%eax\n"
+        "xchgl %%eax, %1\n"
+        "movl %%eax, %0 "
+        :"=m"(output)
+        :"m"(adress)
+        :"eax"
+        );
+        */
+    return output;
+
+}
+
+
+void *SomTest(){
+    for(int i =0; i<1000;i++ ){
+        while(lock(mut)==1){}
+        *count++;
+        unlock(mut);
+    }
+    return NULL;
+}
+
+int main(int argc,char *argv[]){
+    printf("mais print! putain!\n");
+
+    omega = 1;
+    int n_of_th = atoi(argv[1]);
+    pthread_t threadsPhi[n_of_th];
+    printf("1\n");
+    printf("%d", n_of_th);
+
+    for (int i=0; i<n_of_th; i++){
+        pthread_create(&threadsPhi[i],NULL,SomTest,NULL);
+    }
+    printf("2");
+
+    for(int i=0;i<n_of_th;i++) {
+        pthread_join(threadsPhi[i], NULL);
+    }
+    printf("result= %d", *count);
+
     return 0;
-}
-int lock(void* mutexAdress){
-    void *adress= mutexAdress;
-    int output;
-
-    asm ("movl $1, %0;xchgl %0, %1;"
-    :"=r"(output)  /* y is output operand */
-    :"r"(adress)   /* x is input operand */
-    :); /* %eax is clobbered register */
-    printf("adress= %s, output= %d", adress, output);
-    return output;
-}
-
-int unlock(void* mutexAdress){
-
-    void *adress= mutexAdress;
-    int output;
-
-    asm ("movl $0, %0;xchgl %0, %1;"
-    :"=r"(output)  /* y is output operand */
-    :"r"(adress)   /* x is input operand */
-    :);
-
-}
-
-int lock2(){
-    int output;
-    return output;
-}
-
-
-
-int unlock2(){
-    int output;
-    return output;
 }
--- Philosophe.c
+++ Philosophe.c
@@ -9,13 +9,14 @@
 
 
 int n_of_philo;
+int n_of_philoM;               // usefull if only eating alone
 int totalEat;                  //Total of finished Threads
 
 pthread_mutex_t *mutexTotal;
 pthread_mutex_t *mutexBa;
 
 void penser(int number){
-    printf("Philosophe %d pense \n", number+1);
+
 }
 
 /*
@@ -24,64 +25,62 @@ void penser(int number){
 void *gaucher(void *numbers){
     int itt=1;
     int number= (int) numbers;
-    while(itt<=10000) {
+    while(itt<=1000) {
         penser(number);
         pthread_mutex_lock(&mutexBa[number]);
-        pthread_mutex_lock(&mutexBa[(number + 1) % n_of_philo]);
-        printf("philosophe gaucher %d mange %d \n", number+1, itt);
+        pthread_mutex_lock(&mutexBa[(number + 1) % n_of_philoM]);
         pthread_mutex_unlock(&mutexBa[number]);
-        pthread_mutex_unlock(&mutexBa[(number + 1) % n_of_philo]);
+        pthread_mutex_unlock(&mutexBa[(number + 1) % n_of_philoM]);
         itt++;
     }
     pthread_mutex_lock(mutexTotal);
     totalEat++;
     pthread_mutex_unlock(mutexTotal);
-    printf("philosophe %d a fini !\n",number+1);
 }
 
 /*
- * think then eat, Right handed (start by taking the right wand)
+ * think then eat, Right handed (start by taking the right chopstick)
  */
 void *droitier(void *numbers){
     int itt=1;
     int number= (int) numbers;
     while(itt<=10000) {
         penser(number);
-        pthread_mutex_lock(&mutexBa[(number+1)%n_of_philo]);
+        pthread_mutex_lock(&mutexBa[(number+1)%n_of_philoM]);
         pthread_mutex_lock(&mutexBa[number]);
-        printf("philosophe droitier %d mange %d \n", number+1, itt);
-        pthread_mutex_unlock(&mutexBa[(number+1)%n_of_philo]);
+        pthread_mutex_unlock(&mutexBa[(number+1)%n_of_philoM]);
         pthread_mutex_unlock(&mutexBa[number]);
         itt++;
     }
     pthread_mutex_lock(mutexTotal);
     totalEat++;
     pthread_mutex_unlock(mutexTotal);
-    printf("philosophe %d a fini !\n",number+1);
 
 }
 
 
 
-void main(int argc,char *argv[]){
+int main(int argc,char *argv[]){
 
     time_t timed = time(NULL);
 
     n_of_philo=atoi(argv[1]);
-    mutexBa= (pthread_mutex_t *) malloc(sizeof(pthread_mutex_t)*n_of_philo);
+    n_of_philoM=n_of_philo;
+    if(n_of_philo==1){n_of_philoM=2;}
+    mutexBa= (pthread_mutex_t *) malloc(sizeof(pthread_mutex_t)*n_of_philoM);
     pthread_t threadsPhi[n_of_philo];
-    printf("number of philos: %d \n",n_of_philo);
 
-    for(int i=0;i<n_of_philo;i++){
+    for(int i=0;i<n_of_philoM;i++){
         pthread_mutex_init(mutexTotal,NULL);
         pthread_mutex_init(&mutexBa[i],NULL);
+    }
+
+    for(int i=0;i<n_of_philo;i++){
         if(i%2){
             pthread_create(&threadsPhi[i],NULL,gaucher,(void*) i);
-            printf("philo %d crée gaucher \n", i);
         }
         else{
             pthread_create(&threadsPhi[i],NULL,droitier,(void*) i);
-            printf("philo %d crée droitier \n", i);
         }
     }
     for(int i=0;i<n_of_philo;i++) {
@@ -90,6 +89,7 @@ void main(int argc,char *argv[]){
     free(mutexBa);
     printf("total est égal %d\n",totalEat);
     printf("temps = %ld",(time(NULL)-timed) );
+    return 0;
 }
 
 
