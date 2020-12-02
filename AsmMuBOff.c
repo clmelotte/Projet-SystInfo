09c04d1375033d3906b070429449c7e9b82ecaa8
JosephVankelegom <jtevanvan@gmail.com>
2020-12-02 13:04:42 +0100

    mutex Back off avec Test commit !  tout fonctionne sur Clion

--- AsmMuBOff.c
+++ AsmMuBOff.c
@@ -11,12 +11,12 @@
 
 
 
-int createBOff(mutexBOff_t mutBOff,int* mutexAdress, int denIni, int denFin){
+int createBOff(mutexBOff_t *mutBOff,int* mutexAdress, int denIni, int denFin){
     int output;
     if(mutexAdress==NULL || denIni<=0 || denFin < denIni){return 1;}
-    mutBOff.mutexAdress=mutexAdress;
-    mutBOff.denIni=denIni;
-    mutBOff.denFinal=denFin;
+    mutBOff->mutexAdress=mutexAdress;
+    mutBOff->denIni=denIni;
+    mutBOff->denFinal=denFin;
     asm ("movl $0, %%eax\n"
          "xchgl %%eax, (%1)\n"
          "movl %%eax, %0"
@@ -29,10 +29,10 @@ int createBOff(mutexBOff_t mutBOff,int* mutexAdress, int denIni, int denFin){
 
 
 
-int lockBOff(mutexBOff_t mutBOff){
+int lockBOff(mutexBOff_t *mutBOff){
     int output=0;
-    int denIni = mutBOff.denIni;
-    int denFin = mutBOff.denFinal;
+    int denIni = mutBOff->denIni;
+    int denFin = mutBOff->denFinal;
     while(lockcheckBOff(mutBOff)){
         while(rand() > RAND_MAX/denIni){}
         denIni=denIni*2;
@@ -44,9 +44,9 @@ int lockBOff(mutexBOff_t mutBOff){
 /*
  * Output: 0 if can continue or 1 if it can't.
  */
-int lockcheckBOff(mutexBOff_t mutBOff){
+int lockcheckBOff(mutexBOff_t *mutBOff){
     int output;
-    int* mutexAdress = mutBOff.mutexAdress;
+    int* mutexAdress = mutBOff->mutexAdress;
     asm ("1:\n"
          "movl $1, %%eax\n"
          "xchgl %%eax, (%1)\n"
@@ -58,9 +58,9 @@ int lockcheckBOff(mutexBOff_t mutBOff){
     return output;
 }
 
-int unlockBOff(mutexBOff_t mutBOff){
+int unlockBOff(mutexBOff_t *mutBOff){
     int output;
-    int* mutexAdress = mutBOff.mutexAdress;
+    int* mutexAdress = mutBOff->mutexAdress;
 
     asm("movl $0 , %%eax\n"
         "xchgl %%eax, (%1)\n"
---
+++ TestAsmMuBOff.c
@@ -0,0 +1,59 @@
+
+
+//
+// Created by josep on 02/12/2020.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include "AsmMuBOff.h"
+
+int count ;
+
+void *SomTest(void* mutx){
+
+    mutexBOff_t *mut = (mutexBOff_t *) mutx;
+    for(int i =0; i<6400;i++ ){
+        lockBOff(mut);
+        //while(rand() > RAND_MAX/10000){}
+        count++;
+        unlockBOff(mut);
+    }
+    return NULL;
+}
+
+int main(int argc,char *argv[]){
+    if (argc != 4){printf("Error : Wrong number of arguments, "
+                          "3 arguments is expected: nbr of threads, den ini, den max\n");
+        exit(-1);
+    }
+
+    count =0;
+
+
+    int n_of_th = atoi(argv[1]);
+    int denIni = atoi(argv[2]);
+    int denMax = atoi(argv[3]);
+    pthread_t threadsPhi[n_of_th];
+
+
+    mutexBOff_t* mut =(mutexBOff_t *) malloc(sizeof(mutexBOff_t));
+    mut->mutexAdress = (int*) malloc(sizeof(int));
+
+    createBOff(mut, mut->mutexAdress, denIni, denMax);
+
+    for (int i=0; i<n_of_th; i++){
+        pthread_create(&threadsPhi[i],NULL,SomTest,(void*) mut);
+    }
+
+    for(int i=0;i<n_of_th;i++) {
+        pthread_join(threadsPhi[i], NULL);
+    }
+    free(mut->mutexAdress);
+    free(mut);
+    if(count == 6400*n_of_th){return 0;}
+    else {return -1;}
+
+}
+
